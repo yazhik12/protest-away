2bb8c8f3e7dfa7b8209967f5807ee7ed22c517eb
Abha Vedula <abhavedula@abhas-mbp.lan>
2020-07-16 00:36:39 -0700

    Set up incident reporting form

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,16 +7,22 @@ class Home extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            data: []
+            data: [],
+            tweets: new Map(),
         };
+        this.getTweets = this.getTweets.bind(this);
     }
+
     componentDidMount() {
         fetch("http://localhost:8000/api.json")
             .then(res => res.json())
             .then(res => this.setState({ data: res }))
             .catch(err => console.log(err));
     }
+
+
     render() {
+        console.log(this.state.tweets);
         return (<div className={styles.Main}>
             <h1>Fetch API</h1>
             <div className={styles.Container}>
@@ -37,6 +43,55 @@ class Home extends Component {
                 })}
             </div>
         </div >)
+    }
+
+    getTweets() {
+        // TweetJS.com - Display your tweets on your website using Javascript alone
+        // Copyright 2019 Infinite Loop Development Ltd - InfiniteLoop.ie
+        // Do not remove this notice.
+
+        var TweetJs = {
+            ListTweetsOnUserTimeline : function(screenName, callback) {
+                TweetJs._callApi({
+                        Action: "ListTweetsOnUserTimeline",
+                        ScreenName: screenName
+                    },
+                callback);
+            },
+            Search: function (query, callback) {
+                TweetJs._callApi({
+                    Action: "Search",
+                    Query: query
+                }, callback);
+            },
+            _callApi: function (request, callback) {
+                var xhr = new XMLHttpRequest();
+                URL = "https://www.tweetjs.com/API.aspx"; 
+                xhr.open("POST", URL);
+                xhr.onreadystatechange = function () {
+                    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+                        callback(JSON.parse(xhr.response));
+                    }
+                }
+                xhr.send(JSON.stringify(request));
+            }
+        };
+
+        var hashtags = ["#blacklivesmatter", "#racism", "#policebrutality", "#protest", "#georgefloyd", "#tellblackstories", "#blacktechtwitter", "#saytheirnames", "#nojusticenopeace"];
+        // Mapping from hashtag to array of tweets
+        var tweetsMap = new Map();
+
+        hashtags.forEach(getTweets);
+
+        function getTweets(value, index, array) {
+          TweetJs.Search(value,
+            function (data) {
+                tweetsDict[value] = data;
+                console.log(data);
+            });
+        }
+
+        this.state.tweets = tweetsMap;
     }
 }
 
--- src/pages/ReportAnIncident.js
+++ src/pages/ReportAnIncident.js
@@ -1,20 +1,121 @@
 import styles from '../../src/App.module.scss';
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 
 class Report extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            name: '',
+            city: '',
+            state: '',
+            type: '',
+            date: new Date(),
+            email: '',
+            description: '',
+            files: [],
+        };
+        this.handleChange = this.handleChange.bind(this);
+        this.handleDateChange = this.handleDateChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.getIncidentTypes = this.getIncidentTypes.bind(this);
+
     }
+
+    handleChange = (event) => {
+        let name = event.target.name;
+        let value = event.target.value;
+        this.setState({[name]: value});
+    }
+
+    handleDateChange = date => {
+        this.setState({date: date});
+      };
+
+    handleFileChange = (event) => {
+        this.setState({files: event.target.files});
+    }
+
+    handleSubmit(event) {
+        event.preventDefault();
+        //TODO: Implement submit 
+    }
+
+    getIncidentTypes() {
+        return ['Cop Watch', 
+                'Police Accountability',
+                'Corporate Accountability',
+                'Criminal Justice Policy',
+                'Education',
+                'Employment Discrimination',
+                'Wrongful Imprisonment',
+                'Racist Advertisement',
+                'Media Coverage',
+                'Immigration',
+                'Economic Justice',
+                'Other',
+                ];
+    }
+
     render() {
+        var types = this.getIncidentTypes();
         return (
             <div>
                 <h1 className={styles.Header}>
-                    Report an Incident page
+                    Report an Incident
                 </h1>
+                <form onSubmit={this.handleSubmit}>
+                    <label>
+                        Name (Optional)<br />
+                        <input name="name" type="text" onChange={this.handleChange} />
+                    </label>
+                    <br /><br />
+                    <label>
+                        City<br />
+                        <input name="city" type="text" onChange={this.handleChange} />
+                    </label>
+                    <br /><br />
+                    <label>
+                        State<br />
+                        <input name="state" type="text" onChange={this.handleChange} />
+                    </label>
+                    <br /><br />
+                    <label>
+                        Type of Incident<br />
+                        <select name="type" onChange={this.handleChange} >
+                            {types.map((type) => <option value={type}>{type}</option>)}
+                        </select>
+                    </label>
+                    <br /><br />
+                    <label>
+                        Incident Date<br />
+                        <DatePicker selected={this.state.date} onChange={this.handleDateChange} />
+                    </label>
+                    <br /><br />
+                    <label>
+                        Email<br />
+                        <input name="email" type="text" onChange={this.handleChange} />
+                    </label>
+                    <br /><br />
+                    <label>
+                        Description of Incident<br />
+                        <textarea name="description" onChange={this.handleChange} />
+                    </label>
+                    <br /><br />
+                    <label>
+                        Upload Image or Video<br />
+                        <input type="file" onChange={this.handleFileChange}/>
+                    </label>
+                    <br /><br />
+                    <input type="submit" value="Submit" />
+                </form>  
             </div>
         )
     }
+
 }
 
 export default Report;
