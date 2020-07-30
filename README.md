a1b25024cc384e27ac5dc1779cee3cecb6d7f322
khourysmith <khoury.smith@gmail.com>
2020-07-29 20:21:44 -0400

    added images

--- README.md
+++ README.md
@@ -4,15 +4,37 @@
 How do we enable people at risk of COVID-19 or unable to protest in person to support the movement through
 tech?
 
+## Why Does This Matter
+Physical presence as a protest is a lot more risky for people at risk of getting COVID. This includes the elderly population, people with underlying conditions, or people who live with someone who is at-risk. (Non at risk individuals can also use this tool.) CDC reports “An estimated 60 percent of American adults have at least one chronic medical condition.” (Source: https://www.cdc.gov/media/releases/2020/p0625-update-expands-covid-19.html) This is a huge population of people who are now putting themselves at risk by protesting anyways, or keeping themselves from participating in activism.
+
+How do we keep people connected to issues that matter to them, even if they cannot go out and protest? How do we keep people engaged in activism after the protest is over?
+
+## Assumptions
+This is not a replacement of protest, but supports it.
+
 ## The Solution
-A digital platform, called Protest Away, that empowers people to engage in activism by reporting incidents and taking
-action.
+A web application that uses 2 types of data 1. aggregated data about trending hashtags from Twitter (hashtags related to BLM movement/black issues) and 2. incidents reported on our website, to link people who cannot protest to actions they can take from their homes
+
+Our tool offers ways of tackling social, political, and economic activism.
+
+## Key User Group
+1 People who want to report a problem 2 People who don't know how to help/cant help bc of COVID (our focus user for the presentation) 3 Civic organizations who want to get connected with and act on local occurrences of racism
+
+## Technical Resources
+https://stackoverflow.com/questions/51689113/instagram-get-posts-with-hashtag JS library for getting Twitter posts by hashtag https://www.tweetjs.com/ https://stackoverflow.com/questions/50147108/twitter-api-searching-by-hashtag
 
 ## User Stories
 
 - As a user I want to Report incidents of racism directly to our platform. 
 - As a user I want to Select an incident and find out how you can take actionable steps to get justice.
 - As a user I want to Find civic groups in your community to get involved with.
+
+### With reported incidents
+1.As a User, when I arrive at the first page I should be able to see the trending hashtags- the top 5 from Twitter ,.The set of hashtags in consideration will be predefined. 
+
+2.As a User, I will see an updated scream when the screen refreshes 
+
+3.As a User, I should be able to query according to a search criteria and read incidents reported. When i click on a particular record, I should be able to read the entire description
 
 
 
--- src/pages/Connect.js
+++ src/pages/Connect.js
@@ -48,9 +48,7 @@ class Connect extends Component {
         } 
         this.setState({ shownOrgs: newOrgs });        
     }
-     
     render() {
-     
     return (
         <div className={styles.main}>
         {/* <h1 className={styles.Header}>Connect for Black Lives</h1>
@@ -84,97 +82,12 @@ class Connect extends Component {
                       <p>
                           <a href={item.website}>
                             Visit Website
-                             </a>
-                        </p>
+                          </a>
+                      </p>
               </div>
             </div>
           );
         })}
-
-            
-           {/*<div class={connectstyles.item3}>
-                <img
-                src={aclu}
-                alt="American Civil Liberties"
-                height={62}
-                width={93}/>
-            </div>
-            <div class={connectstyles.item4}>
-                <strong>American Civil Liberties</strong>
-                <p>Location: New York, NY</p>
-                <p>
-                    <a href="https://www.aclu.org/affiliate/new-york">
-                Visit Website
-                </a>
-                </p>
-            </div>
-            <div class={connectstyles.item5}>
-                <img src={blm} alt="Black Lives Matter" height={92} width={92} />
-            </div>
-            <div class={connectstyles.item6}>
-                <strong>Black Lives Mater</strong>
-                <p>Location: Multiple</p>
-                <p>
-                    <a href="https://blacklivesmatter.com/">Visit Website</a>
-                </p>
-          </div>
-          <div class={connectstyles.item7}>
-            <img
-              src={bv}
-              alt="Black Visions Collective"
-              height={92}
-              width={92}/>
-          </div>
-          <div class={connectstyles.item8}>
-            <strong>Black Visions Collective</strong>
-            <p>Location: Minneapolis, MN</p>
-            <p>
-              <a href="https://www.blackvisionsmn.org/">Visit Website</a>
-            </p>
-          </div>
-          <div class={connectstyles.item9}>
-            <img
-              src={naacp}
-              alt="NAACP Legal Defense Fund"
-              height={93}
-              width={93}/>
-          </div>
-          <div class={connectstyles.item10}>
-            <strong>NAACP Legal Defense Fund</strong>
-            <p>Location: New York, NY</p>
-            <p>
-              <a href="https://www.naacpldf.org/">Visit Website</a>
-            </p>
-          </div>
-          <div class={connectstyles.item11}>
-            <img
-              src={nbf}
-              alt="NAACP Legal Defense Fund"
-              height={93}
-              width={93}
-            />
-          </div>
-          <div class={connectstyles.item12}>
-            <div class={connectstyles.item11}>
-              <img src={rtb} alt="Reclaim the Block" height={92} width={92} />
-            </div>
-          </div>
-          <div class={connectstyles.item13}>
-            <strong>National Bail Fund Network</strong>{" "}
-            <p>Location: Multiple</p>
-            <p>
-              <a href="https://www.communityjusticeexchange.org/nbfn-directory">
-                Visit Website
-              </a>
-            </p>
-          </div>
-          <div class={connectstyles.item14}>
-            <strong>Reclaim the Block</strong>
-            <p>Location: Minneapolis, MN</p>
-            <p>
-              <a href="https://www.reclaimtheblock.org/home">Visit Website</a>
-            </p>
-          </div>*/}
         </div>
       </div>
     );
