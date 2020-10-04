095ce800d2319141cf4d262636b4dccb6db71c09
Jaime Pereira <jaime@iguana-farm.com>
2020-10-04 12:57:13 -0400

    Update api.js
    fix operator

--- api.js
+++ api.js
@@ -19,7 +19,7 @@ app.use(bodyParser.json());
 
 //Database Config .env
 const config = {
-  host: process.env.PG_HOST ?? 'localhost' ,
+  host: process.env.PG_HOST ? process.env.PG_HOST : 'localhost' ,
   user: process.env.PG_USER,
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASS,
