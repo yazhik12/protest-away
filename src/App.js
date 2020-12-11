3747d2caee122fd1adcda5777b2c2a2d993af7c1
khourysmith <khoury.smith@gmail.com>
2020-12-10 21:15:04 -0500

    restyled the home page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React, { Component } from "react";
 import styles from "./App.module.scss";
-import Main from './Main';
-import { Link } from 'react-router-dom';
+import Main from "./Main";
+import { Link } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className={styles.Navbar}>
-          <Link className={styles.link} to="/"><div><h5 className={styles.NavbarHeaderText}>TAKE ACTION, <strong className={styles.Bold}>FROM HOME.</strong></h5></div></Link>
+          <Link className={styles.link} to="/">
+            <div>
+              <h5 className={styles.NavbarHeaderText}>
+                TAKE ACTION, <strong className={styles.Bold}>FROM HOME.</strong>
+              </h5>
+            </div>
+          </Link>
           <div className={styles.NavbarLinks}>
-            <Link className={styles.link} to="/report"><div className={styles.NavbarLink}><h5>REPORT</h5></div></Link>
-            <Link className={styles.link} to="/connect"><div className={styles.NavbarLink}><h5>CONNECT</h5></div></Link>
+            <Link className={styles.link} to="/connect">
+              <div className={styles.NavbarLink}>
+                <h5>CONNECT TO A CAUSE</h5>
+              </div>
+            </Link>
+            <Link className={styles.link} to="/report">
+              <div className={styles.NavbarLink}>
+                <button>
+                  <h5>REPORT INCIDENT</h5>
+                </button>
+              </div>
+            </Link>
           </div>
         </div>
         <Main />
